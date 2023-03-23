3d87ea3a9fa04a3c060c1ae87df1687482677c3a
Abhishek Jha <abhishekjha96500@gmail.com>
2023-03-23 12:10:27 +0530

    day-2-javascript

---
+++ clock/script.js
@@ -0,0 +1,23 @@
+const minutesHand = document.querySelector('.minute-hand');
+const secondsHand = document.querySelector('.second-hand');
+const hoursHand = document.querySelector('.hour-hand');
+function setDate(){
+ const now = new Date();
+
+ const seconds = now.getSeconds();
+ const secondsDegrees = ((seconds / 60) * 360) + 90;
+ 
+ secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+ const minutes = now.getMinutes();
+ const minutesDegrees = ((minutes / 60) * 360) + 90;
+ 
+ minutesHand.style.transform =`rotate(${minutesDegrees}deg)`;
+ 
+ const hours = now.getHours();
+ const hoursDegrees = ((hours / 12) * 360) + 90;
+
+ hoursHand.style.transform =`rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
